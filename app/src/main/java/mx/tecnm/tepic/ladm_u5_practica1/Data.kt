af6faf8faf1f850e4c0ef6b5928f93f9f9f42b07
Darh98 <72527868+Darh98@users.noreply.github.com>
2021-01-26 22:53:44 -0700

    Final

--- app/src/main/java/mx/tecnm/tepic/ladm_u5_practica1/Data.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u5_practica1/Data.kt
@@ -6,19 +6,18 @@ class Data{
     var nombre : String = ""
     var posicion1 : GeoPoint = GeoPoint(0.0, 0.0)
     var posicion2 : GeoPoint = GeoPoint(0.0, 0.0)
-    var contiene : String = ""
 
     override fun toString(): String {
         return nombre+"\n"+posicion1.latitude+","+posicion1.longitude+"\n"+
-                posicion2.latitude+","+posicion2.longitude + "\n" + contiene
+                posicion2.latitude+","+posicion2.longitude
     }
 
     fun estoyEn(posicionActual: GeoPoint) : Boolean {
 
-        if (posicionActual.latitude >= posicion1.latitude &&
-            posicionActual.latitude <= posicion2.latitude){
-            if (invertir(posicionActual.longitude) >= invertir(posicion1.longitude) &&
-                invertir(posicionActual.longitude) <= invertir(posicion2.longitude)
+        if (posicionActual.latitude <= posicion1.latitude &&
+            posicionActual.latitude >= posicion2.latitude){
+            if (invertir(posicionActual.longitude) <= invertir(posicion1.longitude) &&
+                invertir(posicionActual.longitude) >= invertir(posicion2.longitude)
             ) {
                 return true
             }
